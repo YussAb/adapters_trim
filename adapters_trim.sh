bd1946c9b36a6ded1db7b7b9e573caa3ccd97034
Youssef Abili <youssef.abili@iit.it>
2020-10-26 14:10:57 +0100

    used new names to define output

--- adapters_trim.sh
+++ adapters_trim.sh
@@ -1,8 +1,14 @@
 #! bin/bash
+set -e
+set -u
+set -o pipefail
 
+#web_reference
+#http://www.usadellab.org/cms/?page=trimmomatic
+#http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/TrimmomaticManual_V0.32.pdf
 
-WORK_DIR=/home2/yabili/germline/analysis
-DATA_DIR=/home2/yabili/germline/data
+WORK_DIR=/home2/yabili/adapters_trim/analysis
+DATA_DIR=/home2/yabili/adapters_trim/data
 fwd_read=$1
 rws_read=$2
 
@@ -11,19 +17,16 @@ cd $WORK_DIR
 echo "the current WORK_DIR is..." pwd
 echo "*************************************************************************************"
 
-#http://www.usadellab.org/cms/?page=trimmomatic
-#http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/TrimmomaticManual_V0.32.pdf
-
 echo "Trimmomatic is starting .. "
 echo "*************************************************************************************"
 
 java -jar /apps/bio/trimmomatic/0.38/trimmomatic-0.38.jar PE -threads 16  -phred33  -trimlog log_trim \
- $DATA_DIR/$fwd_read.fastq \
- $DATA_DIR/$rws_read.fastq \
- $fwd_read.paired.fastq \
- $fwd_read.unpaired.fastq \
- $rws_read.paired.fastq  \
- $rws_read.unpaired.fastq\
+ $DATA_DIR/$fwd_read.fastq.gz \
+ $DATA_DIR/$rws_read.fastq.gz \
+ $fwd_read.trimmed.fastq.gz \
+ $fwd_read.unpaired.fastq.gz \
+ $rws_read.trimmed.fastq.gz  \
+ $rws_read.unpaired.fastq.gz \
  ILLUMINACLIP:$DATA_DIR/adapters/NexteraPE-PE.fa:2:30:10 SLIDINGWINDOW:4:15 MINLEN:20 LEADING:3 TRAILING:3 
 
 #cd WORK_DIR
@@ -43,11 +46,17 @@ fi
 
 cd $WORK_DIR/fastqc
 
-fastqc -o $WORK_DIR/fastqc -t 6 $DATA_DIR/$fwd_read.fastq  
-#fastqc $DATA_DIR/$rws_read.fastq 
-fastqc -o $WORK_DIR/fastqc -t 6 $WORK_DIR/$fwd_read.paired.fastq 
-#fastqc $fwd_read.unpaired.fastq 
-#fastqc $rws_read.paired.fastq  
-#fastqc $rws_read.unpaired.fastq
+fastqc -o $WORK_DIR/fastqc -t 6 $DATA_DIR/$fwd_read.fastq.gz
+
+fastqc -o $WORK_DIR/fastqc -t 6 $DATA_DIR/$rws_read.fastq .gz
+
+fastqc -o $WORK_DIR/fastqc -t 6 $WORK_DIR/$fwd_read.trimmed.fastq.gz 
+
+fastqc -o $WORK_DIR/fastqc -t 6 $WORK_DIR/$fwd_read.unpaired.fastq.gz
+
+fastqc -o $WORK_DIR/fastqc -t 6 $WORK_DIR/$rws_read.trimmed.fastq.gz
+
+fastqc -o $WORK_DIR/fastqc -t 6 $WORK_DIR/$rws_read.unpaired.fastq.gz
+
 
 
